10210016b7d4b1224711d1d44ceae847fc953236
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-07-11 16:50:21 +0500

    html css react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,176 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+// import { Link } from "react-router-dom";
+import logo from "./Images/ima.jpg";
+import logo1 from "./Images/code.png";
+import logo2 from "./Images/computer.png";
+import logo3 from "./Images/shopping-bag.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <React.Fragment>
+      <div className="banner">
+        <header>
+          <a href="#" className="logo">
+            Brand Name
+          </a>
+          <div className="toggle"></div>
+        </header>
+        <img src={logo} alt="Logo" />
+        <div className="content">
+          <h2>Success by design</h2>
+          <p>
+            lorem ipsum lorem import PropTypes from 'prop-types'; import
+            PropTypes from 'prop-types'; import React from 'react';;
+          </p>
+        </div>
+      </div>
+      <section className="about">
+        <div className="contextBx">
+          <h2 className="heading">About Us</h2>
+          <p className="col">
+            a book, set of books, optical disc, mobile device, or online
+            informational resource containing articles on various topics,
+            usually in alphabetical arrangement, covering all branches of
+            knowledge or, less commonly, all aspects of one subject.
+            <br></br>a book, set of books, optical disc, mobile device, or
+            online informational resource containing articles on various topics,
+            usually in alphabetical arrangement, covering all branches of
+            knowledge or, less commonly, all aspects of one subject.
+          </p>
+        </div>
+        <div className="imgBx"></div>
+      </section>
+      <section className="services">
+        <h2 className="heading">Our Services</h2>
+        <p className="text">
+          a book, set of books, optical disc, mobile device, or online
+          informational resource containing articles on various topics, usually
+          in alphabetical arrangement, covering all branches of knowledge or,
+          less commonly, all aspects of one subject.
+          <br></br>a book, set of books, optical disc, mobile device, or online
+          informational resource containing articles on various topics, usually
+          in alphabetical arrangement, covering all branches of knowledge or,
+          less commonly, all aspects of one subject.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="container">
+          <div className="serviceBx">
+            <div>
+              <img src={logo1} alt="logo1" />
+              <h2>Design</h2>
+            </div>
+          </div>
+          <div className="serviceBx">
+            <div>
+              <img src={logo2} alt="logo2" />
+              <h2>Development</h2>
+            </div>
+          </div>
+          <div className="serviceBx">
+            <div>
+              <img src={logo3} alt="logo3" />
+              <h2>Branding</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="technology">
+        <div className="contentBx">
+          <h2>We use cuttin edge technology</h2>
+          <p className="text">
+            a book, set of books, optical disc, mobile device, or online
+            informational resource containing articles on various topics,
+            usually in alphabetical arrangement, covering all branches of
+            knowledge or, less commonly, all aspects of one subject.
+            <br></br>a book, set of books, optical disc, mobile device, or
+            online informational resource containing articles on various topics,
+            usually in alphabetical arrangement, covering all branches of
+            knowledge or, less commonly, all aspects of one subject.
+          </p>
+        </div>
+        <div className="imgBx">
+          <img src={logo3} alt="logo3"></img>
+        </div>
+      </section>
+      <section className="client">
+        <h2 className="heading">Our Client</h2>
+        <p className="text">
+          a book, set of books, optical disc, mobile device, or online
+          informational resource containing articles on various topics, usually
+          in alphabetical arrangement, covering all branches of knowledge or,
+          less commonly, all aspects of one subject.
+          <br></br>a book, set of books, optical disc, mobile device, or online
+          informational resource containing articles on various topics, usually
+          in alphabetical arrangement, covering all branches of knowledge or,
+          less commonly, all aspects of one subject.
+        </p>
+        <div className="imgBx">
+          <img src={logo2} alt="logo2" />
+          <img src={logo2} alt="logo2" />
+          <img src={logo2} alt="logo2" />
+          <img src={logo2} alt="logo2" />
+        </div>
+      </section>
+      <section className="testinomials">
+        <h2 className="heading">What our client says</h2>
+        <div className="container">
+          <div className="contentBx">
+            <div>
+              <p>
+                a book, set of books, optical disc, mobile device, or online
+                informational resource containing articles on various topics,
+                usually in alphabetical arrangement, covering all branches of
+                knowledge or, less commonly, all aspects of one subject.
+              </p>
+              <br />
+              <h3>SomeOne famous</h3>
+            </div>
+          </div>
+          <div className="contentBx">
+            <div>
+              <p>
+                a book, set of books, optical disc, mobile device, or online
+                informational resource containing articles on various topics,
+                usually in alphabetical arrangement, covering all branches of
+                knowledge or, less commonly, all aspects of one subject.
+              </p>
+              <br />
+              <h3>SomeOne famous</h3>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="contact">
+        <h2 className="heading">Contact us</h2>
+        <p>
+          a book, set of books, optical disc, mobile device, or online
+          informational resource containing articles on various topics, usually
+          in alphabetical arrangement, covering all branches of knowledge or,
+          less commonly, all aspects of one subject.
+        </p>
+      </section>
+      <section className="about">
+        <div className="contentBx">
+          <div className="form">
+            <div className="inputBx">
+              <input type="text" name="name" placeholder="Full Name" />
+            </div>
+            <div className="inputBx">
+              <input type="text" name="name" placeholder="Email Address" />
+            </div>
+            <div className="inputBx">
+              <input type="text" name="name" placeholder="Mobile Mumber" />
+            </div>
+            <div className="inputBx">
+              <textarea placeholder="Write your message here"></textarea>
+            </div>
+            <div className="inputBx">
+              <input type="submit" name="" value="" />
+            </div>
+          </div>
+        </div>
+        <div className="imgBx2"></div>
+      </section>
+    </React.Fragment>
   );
 }
 
